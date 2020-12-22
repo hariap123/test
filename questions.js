0c02a10e61ef0c8315aab34fce17e849210e47f7
hariap123 <76274976+hariap123@users.noreply.github.com>
2020-12-22 16:17:58 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "SQL stands for?",
+    answer: "Structured Query Language",
+    options: [
+      "Structured Query Language",
+      "Sequential Query Language",
+      "Structured Question Language",
+      "Sequential Question Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The following are components of a database except",
+    answer: "reports",
+    options: [
+      "user data",
+      "metadata",
+      "reports",
+      "indexes",
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The following are functions of a DBMS except?",
+    answer: "creating and processing forms",
+    options: [
+      "creating and processing forms",
+      "creating databases",
+      "processing data",
+      "administrating databases"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+    {
+     numb: 6,
+     question: "Helping people keep track of things is the purpose of a(n)",
+     answer: "database",
+     options: [
+       "database",
+       "table",
+       "instance",
+       "relationship"
+     ]
+   },
+
+   {
+    numb: 7,
+    question: "You can add a row using SQL in a database with which of the following?",
+    answer: "INSERT",
+    options: [
+      "ADD",
+      "CREATE",
+      "INSERT",
+      "MAKE"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "The library function used to find the last occurrence of a character in a string is?",
+    answer: "strrchr()",
+    options: [
+      "strnstr()",
+      "laststr()",
+      "strrchr()",
+      "strstr()"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "The keyword used to transfer control from a function back to the calling function is?",
+    answer: "return",
+    options: [
+      "switch",
+      "goto",
+      "go back",
+      "return"
+    ]
+  },
+  
+  {
+    numb: 10,
+    question: "Which of the following is not logical operator?",
+    answer: "&",
+    options: [
+      "&&",
+      "||",
+      "&",
+      "!"
+    ]
+  },
+];
